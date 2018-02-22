666ce1849b63ce295908d07d9b39f2a25e406981
GboZ <gbolesta21@gmail.com>
2018-02-22 20:26:15 +0100

    add actions to buttons

--- app/src/main/java/com/github/gboz/maddevlab/MainActivity.kt
+++ app/src/main/java/com/github/gboz/maddevlab/MainActivity.kt
@@ -1,14 +1,30 @@
 package com.github.gboz.maddevlab
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_enter_menu.setOnClickListener({
+            /**
+             * TODO
+             * add new activity
+             */
+        })
+
+        btn_source_on_github.setOnClickListener({
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Gboz/MadDevLab")));
+        })
     }
+
 
 
 }
