1a3b8cadf77c88405b3a7a0496c6324d43b0daf5
GboZ <gbolesta21@gmail.com>
2018-03-17 11:59:54 +0100

    add logs of lifecycle to activities

--- app/src/main/java/com/github/gboz/maddevlab/MainActivity.kt
+++ app/src/main/java/com/github/gboz/maddevlab/MainActivity.kt
@@ -4,24 +4,56 @@ import android.content.Intent
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btn_enter_menu.setOnClickListener({
-            startActivity(Intent(Intent(this, TrialsActivity::class.java)))
+            startActivity(Intent(this, TrialsActivity::class.java))
         })
 
         btn_source_on_github.setOnClickListener({
             startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Gboz/MadDevLab")));
         })
+
+        Log.d(TAG, "onCreate")
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
 
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
 
 }
--- app/src/main/java/com/github/gboz/maddevlab/TrialsActivity.kt
+++ app/src/main/java/com/github/gboz/maddevlab/TrialsActivity.kt
@@ -4,11 +4,14 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 
 import android.os.Bundle
+import android.util.Log
 
 import com.github.gboz.maddevlab.trials.DiceActivity
 import kotlinx.android.synthetic.main.activity_trials.*
 
 class TrialsActivity : AppCompatActivity() {
+
+    val TAG = "TrialsActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +21,37 @@ class TrialsActivity : AppCompatActivity() {
             startActivity(Intent(this, DiceActivity::class.java))
         })
 
+        Log.d(TAG, "onCreate")
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
 
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
 
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
 
 }
